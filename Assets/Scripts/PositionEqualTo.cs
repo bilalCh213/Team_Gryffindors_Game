96e036253ad5e0346235ccb2118c211d551bb0c7
Programmer Cheema <47918386+bilalCh213@users.noreply.github.com>
2020-11-28 00:20:10 +0500

    Added Switch Demo.

---
+++ Assets/Scripts/PositionEqualTo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionEqualTo : MonoBehaviour
+{
+    [SerializeField] private Transform tr;
+    
+    void Start()
+    {
+        transform.position = tr.position;
+        transform.rotation = tr.rotation;
+    }
+    
+    void Update()
+    {
+        transform.position = tr.position;
+        transform.rotation = tr.rotation;
+    }
+}
