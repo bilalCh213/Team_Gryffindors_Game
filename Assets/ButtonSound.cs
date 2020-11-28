9f077e01fcf8b731f2c433719594a7ad75028156
Programmer Cheema <47918386+bilalCh213@users.noreply.github.com>
2020-11-28 23:12:21 +0500

    Added and integrated some audio.

---
+++ Assets/ButtonSound.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSound : MonoBehaviour
+{
+    public AudioClip hover;
+    public AudioClip select;
+
+    private bool isNormal = true;
+    private bool isHover = true;
+
+    public AudioSource audSrc;
+
+    public void OnNormalBtn()
+    {
+        isNormal = true;
+    }
+
+    public void OnHoverBtn()
+    {
+        isHover = true;
+
+        if(isNormal)
+            audSrc.PlayOneShot(hover);
+
+        isNormal = false;
+    }
+
+    public void OnSelectBtn()
+    {
+        if(isHover || isNormal)
+            audSrc.PlayOneShot(select);
+
+        isNormal = false;
+        isHover = false;
+    }
+}
--- Assets/Fade.cs
+++ Assets/Fade.cs
@@ -9,6 +9,7 @@ public class Fade : MonoBehaviour
     public bool fadeIn = true;
     public float fadeLerp = 0.001f;
     public string fadeOutScene = "";
+    public AudioSource audSrc;
 
     void Update()
     {
@@ -19,6 +20,7 @@ public class Fade : MonoBehaviour
         else
         {
             GetComponent<Image>().color = Color.Lerp(GetComponent<Image>().color, Color.black, fadeLerp);
+            audSrc.volume = Mathf.Lerp(audSrc.volume, 0.0f, fadeLerp*2.0f);
 
             if(GetComponent<Image>().color == Color.black)
                 SceneManager.LoadScene(fadeOutScene);
