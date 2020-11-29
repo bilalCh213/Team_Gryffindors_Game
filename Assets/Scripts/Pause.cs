07b3e7168169e1ec1cfd417ca717ecf8bbb980d6
Programmer Cheema <47918386+bilalCh213@users.noreply.github.com>
2020-11-29 17:54:42 +0500

    Added Pause state. Decrease Gameover Fade time. Decreased Level 2 difficulty.

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    public string input = "";
+    private bool paused = false;
+
+    void Start()
+    {
+        if(Time.timeScale <= 0.0f) Time.timeScale = 1.0f;
+    }
+
+    void Update()
+    {
+        if(Input.GetButtonDown(input))
+        {
+            if(!paused)
+            {
+                transform.GetChild(0).gameObject.SetActive(true);
+                Time.timeScale = 0.0f;
+            }
+            else
+            {
+                transform.GetChild(0).gameObject.SetActive(false);
+                Time.timeScale = 1.0f;
+            }
+
+            paused = !paused;
+        }
+    }
+}
